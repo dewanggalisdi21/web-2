2eda5e985681f8305d70666bc7afdc1cb309ca8a
dewanggalisdi21 <dewanggaxd@gmail.com>
2025-02-27 11:33:55 +0700

    praktikum 1

---
+++ variable_user.php
@@ -0,0 +1,10 @@
+<?php
+// mendefinisikan variable
+$nama = 'Dewangga';
+$umur = 20;
+$berat = 46;
+
+echo 'Nama saya ' . $nama;
+echo '<br/>Umur : ' . $umur . ' claTahun';
+echo '<br/>Berat : ' . $berat;
+?>
