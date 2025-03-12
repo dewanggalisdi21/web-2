015c23f481f1ea14a22c6319acbcc14fe2d4cbd8
dewanggalisdi21 <dewanggaxd@gmail.com>
2025-03-12 09:52:25 +0700

    Praktikum 02

--- variable_konstanta.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('PHI', 3.14);
-$JARI_JARI = 100;
-$Luas = PHI * $JARI_JARI * $JARI_JARI;
-echo 'Nilai PHI adalah: ' . PHI;
-echo '<br/>Luas Lingkaran dengan jari-jari ' . $JARI_JARI . 'adalah' . $luas;
-?>
